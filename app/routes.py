12a41092cb3cfad7cf51265e4c652f265744c478
Yang Liu <>
2019-09-30 20:04:53 -0400

    login form

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.login import LoginForm
 
 
 @app.route("/")
@@ -11,4 +12,13 @@ def index():
         {"author": {"username": "user2"}, "body": "bar"},
     ]
     return render_template("index.html", title="microblog", user=user, posts=posts)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f"Login Requested. Username={form.username.data}")
+        return redirect(url_for("index"))
+    return render_template("login.html", title="Sign In", form=form)
 
