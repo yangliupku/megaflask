f14043c363d198bc9576d589260ee51011fb9c15
Yang Liu <>
2019-09-30 23:10:01 -0400

    users table

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     email = db.Column(db.String(120), unique=True, index=True)
-    password_hass = db.Column(db.String(128))
+    password_hash = db.Column(db.String(128))
 
     def __repr__(self):
         return f"<User {self.username}>"
