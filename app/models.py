e5f5b4707babe18cb02bfd7d0b88bd60fc228433
Yang Liu <>
2019-10-19 15:51:41 -0400

    profile page

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db, login
 from datetime import datetime
+from hashlib import md5
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import UserMixin
 
@@ -19,6 +20,12 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode("utf-8")).hexdigest()
+        return "https://www.gravatar.com/avatar/{}?d=identicon&s={}".format(
+            digest, size
+        )
 
 
 @login.user_loader
--- app/routes.py
+++ app/routes.py
@@ -59,3 +59,14 @@ def register():
         return redirect(url_for("login"))
     return render_template("register.html", form=form)
 
+
+@app.route("/user/<username>")
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {"author": user.username, "body": "test post 1"},
+        {"author": user.username, "body": "test post 2"},
+    ]
+    return render_template("user.html", user=user, posts=posts)
+
