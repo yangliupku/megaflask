e22fa7a80ec446d8886ee76aeddf8c8e0721d517
Yang Liu <>
2019-10-19 17:29:14 -0400

    log to file

--- app/forms.py
+++ app/forms.py
@@ -41,3 +41,13 @@ class EditProfileForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     about_me = TextAreaField("About me", validators=[Length(min=0, max=140)])
     submit = SubmitField("Submit")
+
+    def __init__(self, original_username: str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self._original_username:
+            user = User.query.filter_by(username=username.data).first()
+            if user is not None:
+                raise ValidationError("Username already exists!")
