2196ee59d7a621313ac1b00341399153597328f6
Yang Liu <>
2019-10-18 23:03:13 -0400

    login function

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template, flash, redirect, url_for
-from app.login import LoginForm
+from app.forms import LoginForm
+from flask_login import login_user, current_user
+from app.models import User
 
 
 @app.route("/")
@@ -16,9 +18,17 @@ def index():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f"Login Requested. Username={form.username.data}")
-        return redirect(url_for("index"))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is not None and user.check_password(form.password.data):
+            login_user(user)
+            return redirect(url_for("index"))
+        else:
+            flash(f"Login Failed")
+            return redirect(url_for("login"))
     return render_template("login.html", title="Sign In", form=form)
 
